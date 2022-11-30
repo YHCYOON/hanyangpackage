728141992d8edd327976fb48a56e3f9e195ede62
윤희찬 <yhc920923@gmail.com>
2022-11-30 20:41:43 +0900

    221130

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import loadScript from 'vue-plugin-load-script'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(loadScript).mount('#app')
