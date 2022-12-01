bea80c4545eb661960c8c76e282e87c1865fb6e5
윤희찬 <yhc920923@gmail.com>
2022-12-01 21:05:41 +0900

    new template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import loadScript from 'vue-plugin-load-script'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(store).use(router).use(loadScript).mount('#app')
+// Nucleo Icons
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+
+import materialKit from "./material-kit";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(materialKit);
+app.mount("#app");
